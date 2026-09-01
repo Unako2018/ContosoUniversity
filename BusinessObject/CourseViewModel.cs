fe5a3e56b938ffb9d518f8a071d107c9f77b4a23
Tawanda Bwerinofa (SGT) <Tawanda.Bwerinofa@sanlam.co.za>
2026-09-01 16:45:12 +0200

    Add Business Objects

---
+++ BusinessObject/CourseViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessObject
+{
+    public class CourseViewModel
+    {
+        public int CourseID { get; set; }//Primary Key 
+        public string Title { get; set; }//string ,stores the name or title of the course 
+        public int Credits { get; set; }//Integer , stores the no of credits worth the course 
+
+    }
+}
---
+++ BusinessObject/StudentViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BusinessObject
+{
+    public class StudentViewModel
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<EnrollmentViewModel> Enrollments { get; set; }
+    }
+}
