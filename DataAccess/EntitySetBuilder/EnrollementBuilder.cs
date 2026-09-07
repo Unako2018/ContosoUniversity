7d6e1ec01eb2f88f22c401c3172a4a568192afa0
Tawanda Bwerinofa (SGT) <Tawanda.Bwerinofa@sanlam.co.za>
2026-09-07 08:57:41 +0200

    Add Entitybuilder Tables

---
+++ DataAccess/EntitySetBuilder/EnrollementBuilder.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.EntitySet;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.EntitySetBuilder
+{
+    public class EnrollmentBuilder : IEntityTypeConfiguration<Enrollment>
+    {
+        public void Configure(EntityTypeBuilder<Enrollment> entityBuilder)
+        {
+            entityBuilder.ToTable(nameof(Enrollment));
+            entityBuilder.HasKey(c => c.EnrollmentID);
+
+            entityBuilder.Property(c => c.CourseID).IsRequired();
+            entityBuilder.Property(c => c.StudentID).IsRequired();
+
+
+            entityBuilder
+            .HasOne(e => e.Student)
+            .WithMany(e => e.Enrollments)
+            .HasForeignKey(e => e.StudentID)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            //entityBuilder
+            //.HasOne(e => e.Course)
+            //.WithMany(e => e.Enrollments)
+            //.HasForeignKey(e => e.CourseID)
+            //.OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ DataAccess/EntitySetBuilder/GradeBuilder.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.EntitySet;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.EntitySetBuilder
+{
+    public class GradeBuilder : IEntityTypeConfiguration<Grade>
+    {
+        public void Configure(EntityTypeBuilder<Grade> entityBuilder)
+        {
+            entityBuilder.ToTable(nameof(Grade));
+            entityBuilder.HasKey(c => c.GradeID);
+
+            entityBuilder.Property(c => c.Name).IsRequired();
+
+            entityBuilder.Property(c => c.Name).HasMaxLength(50);
+            entityBuilder.Property(c => c.Description).HasMaxLength(100);
+
+            entityBuilder
+            .HasOne(e => e.GradeSubType)
+            .WithMany(e => e.Gradees)
+            .HasForeignKey(e => e.GradeSubTypeId)
+            .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
