0a2bc4892e4ffa394b15e7a58bd4e28afb6b7af2
miranda <dimrin002@leapschool.org.za>
2026-09-08 15:56:12 +0200

    test CRUD for Course

--- BusinessObject/CourseViewModel.cs
+++ BusinessObject/CourseViewModel.cs
@@ -5,6 +5,5 @@
         public int CourseID { get; set; }//Primary Key 
         public string Title { get; set; }//string ,stores the name or title of the course 
         public int Credits { get; set; }//Integer , stores the no of credits worth the course 
-        public object Course { get; set; }
     }
 }
